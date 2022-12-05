91edaec9a940db4590e8ac34d1f3d203e342ce97
Otto Ruokonen <otto.ruokonen@myy.haaga-helia.fi>
2022-12-05 18:37:49 +0200

    harjoituksia lisatty

---
+++ osa1/harjoittelu/harjoittelu/src/LeftRightTapahtumat.js
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+    const [value, setValue] = useState(10)
+  
+    const hello = (who) => {
+      const handler = () => {
+        console.log('hello', who)
+      }
+      return handler
+    }
+  
+    return (
+      <div>
+        {value}
+        <button onClick={hello('world')}>button</button>
+        <button onClick={hello('react')}>button</button>
+        <button onClick={hello('function')}>button</button>
+      </div>
+    )
+  }
+export default App
